a406b20c48e5b8aa4061b09b0a1632d3241b79b8
Salva Munoz <salva17y2@gmail.com>
2017-02-15 18:19:45 +0100

    Añadidos primeros botones y funciones
    Añadidos los primeros campos y funcionalidad, tal como los botones de
    cancelar y guardar.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+//objeto que observara cada cambio para llevarlo a la base de datos.
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2';
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  task: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController,
+  public alertController: AlertController,
+  public database: AngularFireDatabase) {
+
+    this.task = this.database.list('/agenda');
   }
+createUser(){
+  let newUserModal = this.alertController.create({
+  title: "Nuevo Contacto",
+  message: "Agrega un nuevo Contacto",
+  inputs: [
+    {
+      name: "nombre",
+      placeholder: "Nombre"
+
+    },
+    {
+      name: "telefono",
+      placeholder: "Teléfono"
+    }
+  ],
+  buttons: [
+  {
+    text: "Cancelar",
+    handler: data => {
+      console.log('Cancel Clic');
+    }
+  }
+  {
+    text: "Guardar",
+    handler: data -> {
+      this.user.push({
+        name: data.nombre,
+        phone: data.telefono
+        });
+    }
+  }
+  ]
+  })
+}
+newUserModal.present(newUserModal);
 
 }
